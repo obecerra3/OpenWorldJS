907b167bcef42daa098190d69038ffdfd4e5921f
Ben Walker <bnwlkr@icloud.com>
2019-07-10 14:59:38 -0700

    you now have to login to view the maze

--- site/maze.php
+++ site/maze.php
@@ -2,18 +2,18 @@
 session_start();
 /* DEBUG */
 
-$_SESSION["username"] = "developer";
-$_SESSION["x"] = 0;
-$_SESSION["y"] = 0;
+//$_SESSION["username"] = "developer";
+//$_SESSION["x"] = 0;
+//$_SESSION["y"] = 0;
 
 
 /* DEBUG */
 
 
-//if(!isset($_SESSION["auth"]) || $_SESSION["auth"] === false){
-//  header("location: login.php");
-//  exit;
-//}
+if(!isset($_SESSION["auth"]) || $_SESSION["auth"] === false){
+  header("location: login.php");
+  exit;
+}
 
 
 
@@ -23,7 +23,7 @@ $_SESSION["y"] = 0;
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Collision DEMO</title>
+    <title>The Maze</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <link type="text/css" rel="stylesheet" href="./css/main.css">
@@ -34,16 +34,23 @@ $_SESSION["y"] = 0;
         height: 100%;
         background-color: rgba(0,0,0,0.5);
       }
-
+      #info {
+        position: absolute;
+        top: 10px;
+        width: 100%;
+        text-align: center;
+        z-index: 100;
+        display:block;
+      }
   
     </style>
   </head>
   <body>
     <div id="blocker">
-
-     
-
+     <div id="info"><?php echo $_SESSION["username"]?></div>
     </div>
+    
+    
 
     <script type="module" src="js/maze.js"></script>
   </body>
