451fdac2f103dd813fbed5e231ca70b1e3b2598c
Ben Walker <bnwlkr@icloud.com>
2019-07-30 23:28:08 -0700

    fix style.css

--- html/maze.php
+++ html/maze.php
@@ -26,7 +26,7 @@ session_start();
     <title>The Maze</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="./css/main.css">
+    <link type="text/css" rel="stylesheet" href="style.css">
     <script>var username = "<?php echo $_SESSION["username"]?>" </script>
   </head>
   <body>
