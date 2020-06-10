e36565d72e98cdc509a8f9848904481f358f3cc2
unknown <Administrator@TSC-Loaner4760.ad.gatech.edu>
2020-06-10 14:08:09 -0400

    added object equality check in Utils.js

--- src/js/maze/utils/Utils.js
+++ src/js/maze/utils/Utils.js
@@ -99,6 +99,26 @@ define(["three", "ammo"], (THREE, Ammo) =>
         {
             return new THREE.Vector3(v.x(), v.y(), v.z());
         },
+
+        isEqual: (a , b) =>
+        {
+            var aProps = Object.getOwnPropertyNames(a);
+            var bProps = Object.getOwnPropertyNames(b);
+
+            if (aProps.length != bProps.length) {
+                return false;
+            }
+
+            for (var i = 0; i < aProps.length; i++) {
+                var propName = aProps[i];
+
+                if (a[propName] !== b[propName]) {
+                    return false;
+                }
+            }
+
+            return true;
+        },
     }
     return Utils;
 });
