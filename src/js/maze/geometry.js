cf28b95de7002d9be92df85aba6c9bccb1a6f470
obecerra <obecerra1228@gmail.com>
2024-03-21 13:54:31 -0700

    Update readme, index.html to remove php requirement to start the game

--- src/js/maze/geometry.js
+++
@@ -1,7 +0,0 @@
-define(["three"], (THREE) => {
-  return {
-    // cube: new THREE.CubeGeometry( 200, 200, 200 ),
-    // sky: new THREE.PlaneGeometry( 1600, 1600 ),
-    // sky2: new THREE.SphereGeometry( 3000, 64, 64 )
-  };
-});
